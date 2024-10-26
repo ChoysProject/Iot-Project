064867b2836669aa6bb87620ca3c133aad0cfdc8
choys <charm4860as@gmail.com>
2024-10-26 16:13:30 +0900

    ondo

--- device/tests/debug.py
+++ device/tests/debug.py
@@ -7,7 +7,18 @@ client = mqtt.Client()
 
 def on_message(client, userdata, message):
     payload = message.payload
-    print(f"Topic[{message.topic}]: Payload={payload}")
+    serial, s1, s2, s3, s4, T, C = struct.unpack(">IBBBBhh", payload)
+    active = (s1 >> 7) & 0x1
+    battery = s1 & 0x7f
+    control = (s2 >> 7) & 0x1
+    period = s2 & 0x7f
+
+    print(f"""Topic[{message.topic}]: SERIAL={serial}, BATTERY={battery}%,
+                            ACTIVE={active}, CONTROL={control},
+                            TARGET={T / 100}, CURRENT={C / 100}""")
+
+    # print(f"Topic[{message.topic}]: Payload={payload}")
+
     # serial, s1, s2, s3, s4, T, C = struct.unpack(">IBBBBhh", payload)
     # active = (s1 >> 7) & 0x1
     # battery = s1 & 0x7f
